99cfc005669c66045f88970b1051e287c513f276
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-05-16 10:13:34 +0600

    initial

--- src/components/navigation/clintNavbar.tsx
+++ src/components/navigation/clintNavbar.tsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
+import Logo from '@/assets/image/logo.svg'
 
 
 export default function ClintNav() {
    return(
       <div>
-         <div className="flex flex-row items-center">
-            <div className="flex flex-row items-center">
-               <Image src="" alt="logo" />
+         <div className="flex flex-row items-center justify-between mx-[150px] bg-amber-100 px-4 py-2 rounded-xl mt-2 ">
+            <div className="flex flex-row items-center gap-2">
+               <Image src={Logo} alt="logo" className="w-[30px] " />
                <p>Notes</p>
             </div>
-            <div className="flex flex-row items-center">
+            <div className="flex flex-row items-center gap-4">
                <p>ADD NOTES</p>
                <p>ADD CALENDAR</p>
                <p>CHATS</p>
                <p>TWITT</p>
             </div>
-            <div className="flex flex-row items-center">
+            <div className="flex flex-row items-center gap-4">
                <button>Logon</button>
                <button>Registration</button>
             </div>
