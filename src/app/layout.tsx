af31af0ff7152b6c18416ef3754667f8835a4df8
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-21 12:06:16 +0600

    now project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
-import * as motion from "motion/react-client"
-import { Toaster } from "react-hot-toast";
 
-
-const geistSans = Geist({
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  weight: "100 900",
 });
-
-const geistMono = Geist_Mono({
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -27,17 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     <head>
-      {/* Poppins font */}
-      <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-      </style>
-     </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster/>
       </body>
     </html>
   );
